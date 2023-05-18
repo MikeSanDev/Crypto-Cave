75edfa1b215afaa7b2010b251084608cd389171f
MikeSanDev <mjs070594@gmail.com>
2023-05-17 23:12:23 -0500

    adding navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import { Switch, Route, Link } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
+// To take multiple components from the components folder - create an index.js inside the component folder
+// insert this into index.js => import { default as Navbar } from './Navbar';
+// Then import each component with {} from './components'
+import { Navbar } from './components'
 
 const App = () => {
     return (
-        <div>
-            <h1>Quickly! To the Crypto Cave!</h1>
+        <div className='app'>
+            <div className='navbar'>
+                <Navbar />
+            </div>
+            <div className='main'>
+
+            </div>
+            <div className='footer'>
+
+            </div>
         </div>
     )
 }
